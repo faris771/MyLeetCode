c16e0e0f678a52c926365fe6bca8a2584bc0f9ed
Faris Abufarha <70337488+faris771@users.noreply.github.com>
2022-10-26 21:35:07 +0300

    Time: 308 ms (57.62%), Space: 51.1 MB (31.88%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,21 +1,27 @@
+#include<bits/stdc++.h>
 #define prioq priority_queue
 #define pb push_back
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        map<int,int> mp;
         
+        if(nums.size() == 0 )
+            return 0;
+        
+        
+        map<int,int> mp;
         for(int x : nums) {
             mp[x] = 1;
             
         }
+        
         int i =0 ;
         int prevK;
-        prioq<int> pq;
         int cnt = 1;
-        if(nums.size() == 0 )
-            return 0;
+        int maxm = 1;
+        
+        prioq<int> pq;
         
         for(auto kv : mp) {
             if(i == 0) {
@@ -27,19 +33,21 @@ public:
             if(kv.first - prevK == 1)
                 cnt++;
             else{
-                pq.push(cnt);
+               
                 cnt = 1;
             }
             
-           
+             if(cnt > maxm){
+                    maxm =cnt;
+            }
             
             prevK = kv.first;
             
             i++;
             
         }
-        pq.push(cnt);
-        return pq.top();
+        
+        return maxm;
         
     }
 };
