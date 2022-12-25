5d76f0426a05ca11149675a777ad72778ef40950
Faris Abufarha <70337488+faris771@users.noreply.github.com>
2022-12-25 02:35:52 +0200

    Time: 111 ms (80.42%), Space: 21.1 MB (41.66%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans; 
+        if(nums.size() < 3) return ans;
+        sort(nums.begin(), nums.end());
+        // unordered_set<int> st;
+        
+
+        for(int i =0; i<nums.size()-2; i++ ) {
+            int target = -nums[i];
+            if(i> 0 && nums[i] == nums[i-1])
+                continue;
+
+            int l = i+1 , r = nums.size() - 1;
+            while(l < r ){
+            
+                if(nums[l] + nums[r] < target)
+                    l++;
+                else if(nums[l] + nums[r] > target)
+                    r--;
+                else{
+                    vector<int> tmp = {nums[l],nums[r],-target};
+                    ans.push_back(tmp);
+
+                    l++;
+                    while(nums[l] == nums[l-1]  && l < r)
+                        l ++;
+                }
+
+            }        
+            out:;
+
+
+        }
+        // for(auto x : ans_set){
+        //     ans.push_back(x);
+        // }
+        return ans;
+    }
+};
