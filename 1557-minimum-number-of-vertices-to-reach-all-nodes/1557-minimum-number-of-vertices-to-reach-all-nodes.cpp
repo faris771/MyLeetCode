a7a2e90d673136b76eb0044adb34243d7518334b
Faris Abufarha <70337488+faris771@users.noreply.github.com>
2023-04-23 15:28:44 +0200

    Time: 349 ms (39.14%), Space: 106 MB (55.04%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+    
+        vector<bool> freq(n,0);
+        
+        for(auto x : edges){
+            
+            freq[x[1]] = 1;
+            
+        }
+        vector<int> ans;
+        
+        for(int i = 0 ; i< n; i++){
+            
+            if(freq[i] == 0 )
+                ans.push_back(i);
+            
+        }
+        
+        
+        return ans;
+        
+    }
+};
