5d12617e4eb8061877fc02f8364e9648deec7996
Faris Abu Farha <70337488+faris771@users.noreply.github.com>
2024-05-03 16:38:03 +0200

    Time: 20 ms (85.22%), Space: 32 MB (96.01%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -22,12 +22,12 @@ public:
         if(!root2)
             return root1;
         
-        root1->val += root2->val;
         
         root1->left  = mergeTrees(root1->left, root2->left);
         root1->right  = mergeTrees(root1->right, root2->right);
         
         
+        root1->val += root2->val;
         
     
         return root1;
