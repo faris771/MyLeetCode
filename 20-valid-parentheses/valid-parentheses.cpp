f5a1d8120ddadf34aee42e89bbce8f650dd5ed83
Faris Abu Farha <70337488+faris771@users.noreply.github.com>
2025-02-05 16:16:42 +0200

    Time: 0 ms (100.00%) | Memory: 9 MB (20.81%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+
+        stack<char> open;
+        stack<char> close;
+
+        if(s.length() <=1 )
+            return false;
+
+        for(char c : s){
+            
+            if(c =='(' || c =='{' || c == '['){
+                open.push(c);
+
+            }
+
+            else if (c ==')' || c =='}' || c == ']'  ){
+                close.push(c);
+
+                if(open.empty()){
+                    return false ;
+                }
+
+
+                if(invertPar(close.top()) != open.top()){
+                    return false;
+                }
+
+                // same 
+
+                open.pop();
+                close.pop();
+                
+
+            }
+           
+        }
+
+        return open.empty() & close.empty();
+
+        //     return true;
+        // else
+        //     return false;
+
+        
+    }
+
+
+
+
+    char invertPar(char c){
+        if(c == ')'){
+         
+            return '(';
+        
+        }
+        else if( c == ']'){
+         
+            return '[';
+        
+        }
+        else{
+         
+            return '{';
+        
+        }
+    }
+};
