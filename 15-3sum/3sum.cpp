2cf5e72adf66933c7ff6171faf9ca11572983d34
Faris Abu Farha <70337488+faris771@users.noreply.github.com>
2025-02-08 14:04:43 +0200

    Time: 41 ms (90.23%) | Memory: 29.1 MB (67.87%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        vector<vector<int>> ans;
+
+        std::sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+
+        int l  =0, r = n -1 ;
+
+        for(int i =0; i < n-2;i++){
+            
+            if(i>0 && nums[i] == nums[i-1] )
+                continue;
+
+            int target =  -nums[i];
+
+            l = i +1;
+            r = n-1;
+
+            while(l<r){
+                if(nums[l] + nums[r] == target){
+                    ans.push_back({nums[i],nums[l],nums[r]});
+
+                    while(l<r && nums[l+1] == nums[l]){
+                        l++;
+
+                    }
+                    while(l<r && nums[r-1] == nums[r]){
+                        r--;
+                        
+                    }
+
+                    l++;
+                    r--;
+                
+                }
+                else if (nums[l] + nums[r] < target){
+                    l++;
+                }
+                else{
+                    r--;
+                }
+                
+            }
+
+
+
+        }
+
+        return ans;
+    }
+};
