0655849be9e5579bef446377c9e59dc2135572ed
Faris Abu Farha <70337488+faris771@users.noreply.github.com>
2025-02-14 17:27:35 +0200

    Time: 0 ms (100.00%) | Memory: 25.8 MB (93.75%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-
-        // O(N) memory 
-
-
+        // O(1) memory 
 
         int n = height.size();
         if(n<=1)
             return 0;
+        int l = 0, r = n- 1;
 
-        vector<int> pre (n);
-        vector<int> post (n);
-        pre[0] = 0;
-
+        int maxLeft = height[0] , maxRight = height[n-1];
         int ans = 0;
+        while ( l< r ){
 
-        
-        // fill pre
+            if (height[l]<= height[r]){
+                
+                l++;
+                maxLeft = max(maxLeft,height[l]);
+                ans +=maxLeft - height[l];
 
-        int maxLeftSum = 0;
-        
-        for(int i =1; i < n; i++){
-            maxLeftSum = max(maxLeftSum, height[i-1]);
-            pre[i] = maxLeftSum;
+            }
+            else{
+                r--;
+                maxRight = max(maxRight,height[r]);
+                ans +=maxRight - height[r];
+
+            }
 
         }
 
-        int maxRightSum = 0;
 
-        post[n-1] = 0;
-        for(int i =n-2; i >= 0; i--){
-            maxRightSum = max(maxRightSum, height[i+1]);
-            post[i] = maxRightSum;
-        }
-
-
-        for(int i =0;i<n;i++){
-            ans += max(0 , ( min(pre[i],post[i]) - height[i])  );
-        }
+    
 
         return ans;
     }
