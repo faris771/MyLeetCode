3b944e4afb43c706828edec3ca40a2866c71eaff
Faris Abu Farha <70337488+faris771@users.noreply.github.com>
2025-02-21 00:15:14 +0200

    Time: 0 ms (100.00%) | Memory: 11.9 MB (48.95%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,14 +4,31 @@ public:
         int n = s.length();
         unordered_map<char, int> freq; 
         int l = 0, ans = 0;
+        int r =0;
+        
+        while(r < n){
 
-        for (int r = 0; r < n; r++) {
-            if (freq.count(s[r])) {
-                l = max(l, freq[s[r]] + 1); 
+            if(freq.count(s[r])) { // element found b4
+                l = max(l,freq[s[r]] +1);
             }
-            freq[s[r]] = r; 
-            ans = max(ans, r - l + 1); 
+
+            freq[s[r]] = r;
+
+            ans = max(ans, (r-l +1));
+
+            r++;
         }
+
         return ans;
+
+
+
+        // for (int r = 0; r < n; r++) {
+        //     if (freq.count(s[r])) {
+        //         l = max(l, freq[s[r]] + 1); 
+        //     }
+        //     freq[s[r]] = r; 
+        //     ans = max(ans, r - l + 1); 
+        // }
     }
 };
