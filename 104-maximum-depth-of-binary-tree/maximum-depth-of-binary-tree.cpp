7b00764165d15350d96c466c7ae1501921239ee0
Faris Abu Farha <70337488+faris771@users.noreply.github.com>
2025-02-25 00:33:42 +0200

    Time: 0 ms (100.00%) | Memory: 19.1 MB (44.72%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -12,14 +12,38 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+
+
         if(!root)
             return 0;
+        
+        queue<TreeNode*> q;
+        
+        q.push(root);
+
+        int ans = 0;
+        while(!q.empty()){
+            int currentQueueSize = q.size();
+
+            for(int i = 0; i< currentQueueSize;i++){
+                TreeNode* frontNode = q.front();
+                q.pop();
+
+                if(frontNode->left){
+                    q.push(frontNode->left);
+
+                }
+                if(frontNode->right){
+                    q.push(frontNode->right);
+
+                }
+
+            }
+            ans++;
 
 
-        return 1 + max(maxDepth(root->left),maxDepth(root->right));
+        }
 
-
-
-
+        return ans;
     }
 };
