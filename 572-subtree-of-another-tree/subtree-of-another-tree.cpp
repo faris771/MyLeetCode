a6c53cf323acbbbbcb8c44a750187d1db462b9ef
Faris Abu Farha <70337488+faris771@users.noreply.github.com>
2025-03-06 01:45:37 +0200

    Time: 0 ms (100.00%) | Memory: 28.9 MB (64.53%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -1,39 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(!root || !subRoot)
-            return 0;
-        if(isSameTree(root,subRoot)){
+        if (!subRoot)
             return true;
-        }
+        if(!root)
+            return  false;
 
-        return isSubtree(root->left,subRoot) || \
-        isSubtree(root->right,subRoot);
+        if (isSameTree(root, subRoot)) 
+            return true;
 
-
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
     }
 
-     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
-        if(!p && q || p &&!q)
-            return 0;
-        if(!p && !q)
-            return 1;
-        
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q || p->val != q->val) return false;
 
-        return (p->val == q->val) && isSameTree(p->left,q->left) && \
-        isSameTree(p->right, q->right);
-
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
